d66ce217e8768c3aeb2cb669cfd2d35acb64bb58
Aishwarya <42641684+aishwa8141@users.noreply.github.com>
2019-10-10 17:41:03 +0530

    Merge UI from demo to release-1.1.0  (#60)
    * merge PR 54 to releae-1.1.0 from demo Cert ui: Added tabs (steps) for creating certificate

--- ui-sample/angular-ui/src/app/components/create-certificate/create-certificate.component.ts
+++ ui-sample/angular-ui/src/app/components/create-certificate/create-certificate.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList, OnChanges } from '@angular/core';
 import { DataService } from '../../services/data/data.service';
 import { ResourceService } from '../../services/resource/resource.service';
 import { FormService } from '../../services/forms/form.service';
 import { DefaultTemplateComponent } from '../default-template/default-template.component';
 import * as _ from 'lodash-es';
 import urlConfig from '../../services/urlConfig.json';
-import { CertReq } from '../../services/interfaces/certificate';
+import { CertReq, Store, Templates } from '../../services/interfaces/certificate';
 import { Router } from '@angular/router';
-
 
 
 @Component({
@@ -19,17 +18,25 @@ export class CreateCertificateComponent implements OnInit {
 
   @ViewChild('formData') formData: DefaultTemplateComponent;
   @ViewChildren('signatoryForm') signatoryFormData: QueryList<DefaultTemplateComponent>;
+  @ViewChild('storageForm') storageFormData: DefaultTemplateComponent;
   dataService: DataService;
   formService: FormService;
   resourceService: ResourceService;
-  pdfUrl: String;
+  pdfUrl: string;
   public formFieldProperties: any;
-  public issuerFieldProperties: any;
+  public storeFieldProperties: any;
   public signatoryFieldProperties: any;
   public req: CertReq;
   router: Router
   signatory: Array<Number> = [1];
   signatoryCount = 1;
+  preview: boolean = false;
+  storageInfo: boolean = false;
+  storageDetails = {};
+  templateActive: boolean;
+  htmlTemplate: string;
+  listOfTemplate: Array<Templates> = [];
+  id: number = 1;
   constructor(dataService: DataService, formService: FormService, resourceService: ResourceService, router: Router) {
     this.dataService = dataService;
     this.resourceService = resourceService;
@@ -38,16 +45,28 @@ export class CreateCertificateComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.templateActive = true;
     this.formService.getFormConfig("certificate").subscribe(res => {
       this.formFieldProperties = res.fields;
     });
     this.formService.getFormConfig("signatory").subscribe(res => {
       this.signatoryFieldProperties = res.fields;
     });
+    this.formService.getFormConfig("store").subscribe(res => {
+      this.storeFieldProperties = res.fields;
+    });
+    this.getTemplates();
+  }
 
+
+  getTemplates() {
+    const template = {
+      id: "temp1",
+      name: "v1/certificate.zip"
+    }
+    this.listOfTemplate.push(template);
   }
   createCertificate() {
-    console.log(this.formData.formInputData)
     const certificateData = this.generateData(_.pickBy(this.formData.formInputData));
     const requestData = {
       data: {
@@ -58,18 +77,20 @@ export class CreateCertificateComponent implements OnInit {
       },
       url: urlConfig.URLS.GENERTATE_CERT
     }
-    console.log("form data", requestData)
     this.dataService.post(requestData).subscribe(res => {
       console.log('certificate generated successfully', res)
-      if(res.result.response[0].preview)  {
-        window.open(res.result.response[0].pdfUrl, '_blank');
-      } else  {
-        this.pdfUrl = res.result.response[0].pdfUrl;
+      this.pdfUrl = res.result.response[0].pdfUrl;
+      if (this.pdfUrl.startsWith("http")) {
+        window.open(this.pdfUrl, '_blank');
+      } else {
         this.dowloadPdf();
-
       }
     });
   }
+  /**
+   * 
+   * @param request 
+   */
   generateData(request: any) {
     const signatoryList = [];
     const requestData = _.cloneDeep(request);
@@ -80,7 +101,7 @@ export class CreateCertificateComponent implements OnInit {
       htmlTemplate: '',
       courseName: '',
       name: '',
-      description: '',
+      description: ''
     };
     const data = [{
       recipientName: requestData.recipientName,
@@ -94,14 +115,41 @@ export class CreateCertificateComponent implements OnInit {
     this.signatoryFormData.forEach(issuer => {
       signatoryList.push(issuer.formInputData);
     });
+    if (this.storageInfo) {
+      certificate['storeConfig'] = this.getStorageDetail();
+    }
     certificate.data = data;
     certificate.issuer = issuer;
     certificate.signatoryList = signatoryList;
-    certificate.htmlTemplate = requestData.htmlTemplateUrl;
+    certificate.htmlTemplate = this.htmlTemplate;
     certificate.courseName = requestData.courseName;
     certificate.name = requestData.certificateName;
     certificate.description = requestData.certificateDescription;
+    if (this.preview) {
+      certificate['preview'] = "true";
+      this.preview = false;
+    }
     return certificate;
+  }
+  getStorageDetail() {
+    this.storageDetails = _.pickBy(this.storageFormData.formInputData);
+    const account = {
+      account: '',
+      key: '',
+      path: '',
+      containerName: ''
+    }
+    const storeConfig = {
+      type: ''
+    };
+    const type = this.storageDetails['type'];
+    storeConfig.type = type;
+    account.account = this.storageDetails['account'];
+    account.key = this.storageDetails['key'];
+    account.containerName = this.storageDetails['containerName'];
+    account.path = this.storageDetails['path'];
+    storeConfig[type] = account;
+    return storeConfig;
   }
   addSignatory() {
     this.signatory.push(this.signatoryCount++)
@@ -117,17 +165,21 @@ export class CreateCertificateComponent implements OnInit {
       url: urlConfig.URLS.DOWLOAD_PDF
     }
     this.dataService.post(requestData).subscribe(res => {
-      console.log(res)
       window.open(res.result.signedUrl, '_blank');
       this.router.navigate(['']);
     });
   }
   removeSignatory() {
+    if (this.signatory.length > 1) {
+      this.signatory
+    }
     if (this.signatoryCount > 1) {
       this.signatory.pop()
-      this.signatoryCount --;
+      this.signatoryCount--;
     }
   }
-
+  previewCertificate() {
+    this.preview = true;
+    this.createCertificate();
+  }
 }
-
