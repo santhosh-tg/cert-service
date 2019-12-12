e077d771e109d22f02ae3e8cb77d6263d16364fb
amit <amit.kumar@tarento.com>
2019-12-12 16:01:53 +0530

    Issue #0 feat:poc

--- certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/HeadlessChromeHtmlToPdfConverter.java
+++ certgen/certProcessor/src/main/java/org/incredible/certProcessor/views/HeadlessChromeHtmlToPdfConverter.java
@@ -1,19 +1,23 @@
 package org.incredible.certProcessor.views;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class HeadlessChromeHtmlToPdfConverter {
 
     public static void convert(File htmlFile,File pdfFile) {
+        Process process = null;
         try {
             boolean isWindows = System.getProperty("os.name")
                     .toLowerCase().startsWith("windows");
             Runtime rt = Runtime.getRuntime();
                 if (isWindows) {
-                    rt.exec(new String[]{"cmd.exe", "/c", "google-chrome --headless --print-to-pdf="+pdfFile.getAbsolutePath()+" "+htmlFile.getAbsolutePath()});
+                    process =  rt.exec(new String[]{"cmd.exe", "/c", "chromium-browser --no-sandbox --headless --print-to-pdf="+pdfFile.getAbsolutePath()+" "+htmlFile.getAbsolutePath()});
                 } else {
-                    rt.exec(new String[]{"sh", "-c", "google-chrome --headless --print-to-pdf="+pdfFile.getAbsolutePath()+" "+htmlFile.getAbsolutePath()});
+                    process = rt.exec(new String[]{"sh", "-c", "chromium-browser --no-sandbox --headless --print-to-pdf="+pdfFile.getAbsolutePath()+" "+htmlFile.getAbsolutePath()});
                 }
+            System.out.println("Input stream ::: "+convertInputStreamToString(process.getInputStream()));
+            System.out.println("Error stream ::: "+convertInputStreamToString(process.getErrorStream()));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -25,6 +29,20 @@ public class HeadlessChromeHtmlToPdfConverter {
             File pFile = new File("/home/amit/sunbird/cert-service/service/conf/0125450863553740809_certificate", 43+".pdf");
             convert(hFile, pFile);
        // }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream)
+            throws IOException {
+        if (inputStream == null)
+            return "Empty";
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        inputStream.close();
+        return result.toString(StandardCharsets.UTF_8.name());
     }
 
 }
